e375c92069ee60367ff3ee51d83fdcfdeb945897
AliRanjbarX <ali.ranjbar0151316@gmail.com>
2026-02-19 13:20:46 +0330

    block input and arrangement

--- gittestt/blockArrangement.h
+++ gittestt/blockArrangement.h
@@ -2,18 +2,23 @@
 // Created by ALI on 17/02/2026.
 //
 
-#ifndef GITTESTT_BLOCKARRANGEMENT_H
-#define GITTESTT_BLOCKARRANGEMENT_H
+#ifndef FOP_PROJECT_ALI_BLOCKARRANGEMENT_H
+#define FOP_PROJECT_ALI_BLOCKARRANGEMENT_H
 
 #include "Essentials.h"
 #include "Shapes.h"
 //motion blocks making
 int size_motion = 15;
 vector<block1> menu_block_motion(size_motion);
-
+//looks blocks making
 int size_looks = 17;
 vector<block1> menu_block_looks(size_looks);
-
+//sound block making
+int size_sound = 8;
+vector<block1> menu_block_sound(size_sound);
+//events block making
+int size_events = 8;
+vector<block1> menu_block_events(size_events);
 
 
 
@@ -27,4 +32,4 @@ void arrange(int dist, vector<block1> &blocks){
         blocks[i].y = blocks[i-1].y + dist;
     }
 }
-#endif //GITTESTT_BLOCKARRANGEMENT_H
+#endif //FOP_PROJECT_ALI_BLOCKARRANGEMENT_H
